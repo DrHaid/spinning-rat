05e2ef9af318ebcb3b33390a9cfc490de60c31fc
DrHaid <40427630+DrHaid@users.noreply.github.com>
2023-03-12 15:48:06 +0100

    Base path fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/spinning-rat/",
   plugins: [react(), svgr()],
   assetsInclude: ["**/*.gltf"],
 });
