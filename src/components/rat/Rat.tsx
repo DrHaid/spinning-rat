946f221c49888d19f4eeeaa0202b3e586bfc6a18
DrHaid <40427630+DrHaid@users.noreply.github.com>
2023-05-26 17:04:25 +0200

    rotate rat on each local axis

--- src/components/rat/Rat.tsx
+++ src/components/rat/Rat.tsx
@@ -1,13 +1,18 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useContext, useRef } from "react";
-import { Group } from "three";
+import { Group, Vector3 } from "three";
 import { RatSpinContext } from "../../RatSpinProvider";
 import { RatMesh } from "./RatMesh";
 
 const deg2rad = (degrees: number) => degrees * (Math.PI / 180);
 
 const SPEED_MULTIPLIER = 65;
+const VECTOR = {
+  RIGHT: new Vector3(1, 0, 0),
+  UP: new Vector3(0, 1, 0),
+  FORWARD: new Vector3(0, 0, 1),
+};
 
 export const Rat = () => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -16,9 +21,18 @@ export const Rat = () => {
 
   useFrame((_state, delta) => {
     const speed = convertSpeed(spinParams.speed);
-    ratMesh.current.rotation.x += spinParams.spinX * (speed * delta);
-    ratMesh.current.rotation.y += spinParams.spinY * (speed * delta);
-    ratMesh.current.rotation.z += spinParams.spinZ * (speed * delta);
+    ratMesh.current.rotateOnAxis(
+      VECTOR.RIGHT,
+      spinParams.spinX * (speed * delta)
+    );
+    ratMesh.current.rotateOnAxis(
+      VECTOR.UP, 
+      spinParams.spinY * (speed * delta)
+    );
+    ratMesh.current.rotateOnAxis(
+      VECTOR.FORWARD,
+      spinParams.spinZ * (speed * delta)
+    );
   });
 
   const convertSpeed = (speedParam: number) => {
